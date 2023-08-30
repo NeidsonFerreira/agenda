f902653bb5f4b1d8f759d149030530620ea4db84
Aluno Lab 135 <lab135@lab.senai>
2023-08-30 19:21:16 -0300

    primeira agendat

---
+++ agenda.py
@@ -0,0 +1,19 @@
+import os
+agenda=[]
+sair=False
+while(sair==False):
+    print("Lista de Opções")
+    print("1-Novo Contato")
+    print("2-Listar Contatos")
+    print("3- Editar Contato")
+    print("4-Excluir Contato \n")
+    opcao=int(input("Digite a Opção Escolhida ....:"))
+
+    if(opcao==1):
+        novo_contato=[]
+        nome=input("\nDigite o Nome do Contato:")
+        novo_contato.append(nome)
+        telefone=input("Digite o Telefone do Contato:")
+        novo_contato.append(telefone)
+        agenda.append(novo_contato)
+        os.system('cls')
